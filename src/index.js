4c7369825027f4f19fd4ef68f2a4da4d598f5691
Devon Moubry <devon@moubry.com>
2017-09-23 17:04:06 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import "./styles/App.css";
+import registerServiceWorker from "./registerServiceWorker";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Explorer from "./components/Explorer";
+import People from "./components/People";
+import Starships from "./components/Starships";
+import Films from "./components/Films";
+import Details from "./components/Details";
+import BaseLayout from "./components/Layout";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <BaseLayout>
+      <Switch>
+        <Route exact path="/" component={Explorer} />
+        <Route path="/people" component={People} />
+        <Route path="/starships" component={Starships} />
+        <Route path="/films" component={Films} />
+        <Route path="/details/:endpoint/:id" component={Details} />
+      </Switch>
+    </BaseLayout>
+  </BrowserRouter>
+
+,document.getElementById("root"));
+registerServiceWorker();
